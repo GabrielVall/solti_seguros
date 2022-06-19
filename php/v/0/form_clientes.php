e87813d1ea6dcbeb98d635ce7aff369bf9f216f1
GabrielVall <gabrielvallejo2000@gmail.com>
2022-06-19 17:29:12 -0500

    Form clientes

--- php/v/0/form_clientes.php
+++ php/v/0/form_clientes.php
@@ -25,36 +25,47 @@ if(!strpos($_SERVER['REQUEST_URI'], 'form_clientes') !== false){
     <div class="card-body pt-0">
         <div class="form-horizontal" >
             <div class="form-group">
+            <label for="nombre">Nombre del cliente</label>
                 <input type="text" class="form-control" id="nombre" placeholder="Nombre">
             </div>
             <div class="form-group">
+                <label for="apellido_paterno">Apellido paterno</label>
                 <input type="text" class="form-control" id="apellido_paterno" placeholder="Apellido Paterno">
             </div>
             <div class="form-group">
+                <label for="apellido_materno">Apellido materno</label>
                 <input type="text" class="form-control" id="apellido_materno" placeholder="Apellido Materno">
             </div>
             <div class="form-group">
+                <label for="telefono">Telefono</label>
                 <input type="text" class="form-control" id="telefono" placeholder="Telefono">
             </div>
             <div class="form-group">
+                <label for="correo">Correo electronico</label>
                 <input type="email" class="form-control" id="correo" placeholder="correo">
             </div>
             <div class="form-group">
+                <label for="direccion">Dirección</label>
                 <input type="text" class="form-control" id="direccion" placeholder="Dirección">
             </div>
             <div class="form-group">
+                <label for="estado">Estado</label>
                 <input type="text" class="form-control" id="estado" placeholder="Estado">
             </div>
             <div class="form-group">
+                <label for="ciudad">Ciudad</label>
                 <input type="text" class="form-control" id="ciudad" placeholder="Ciudad">
             </div>
             <div class="form-group">
+                <label for="fax">Fax</label>
                 <input type="text" class="form-control" id="fax" placeholder="Fax">
             </div>
             <div class="form-group">
+                <label for="usuario">Nombre de usuario</label>
                 <input type="text" class="form-control" id="usuario" placeholder="Usuario">
             </div>
             <div class="form-group">
+                <label for="pass">Contraseña</label>
                 <input type="password" class="form-control" id="pass" placeholder="Contraseña">
             </div>
             <div class="form-group mb-0 mt-3 justify-content-end d-flex">
