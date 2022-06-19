c7eee0e5aa0f0784f5d8c6a983af27caa2990238
GabrielVall <gabrielvallejo2000@gmail.com>
2022-06-19 12:50:13 -0500

    Agregar medico

---
+++ php/v/0/form_medicos.php
@@ -0,0 +1,79 @@
+<?php 
+if(session_status() == PHP_SESSION_NONE){
+    session_start();
+    // Si esta declarada la variable de sesion
+}
+include_once('../../m/SQLConexion.php');
+$sql = new SQLConexion();
+$proveedores = $sql->obtenerResultado("CALL sp_select_proveedores()");
+$tipos_medicos = $sql->obtenerResultado("CALL sp_select_tipos_medicos()");
+?>
+<div class="card  box-shadow-0">
+    <div class="card-header">
+        <h4 class="card-title mb-1">Formulario para medicos</h4>
+        <p class="mb-2">Agrega  los datos basicos de un nuevo medico.</p>
+    </div>
+    <div class="card-body pt-0">
+        <div class="form-horizontal" >
+            <div class="form-group">
+                <select class="form-control" id="id_aseguradora">
+                    <option value="">Seleccione una aseguradora</option>
+                    <?php 
+                        foreach($proveedores as $proveedor){
+                            echo '<option value="'.$proveedor['id_proveedor'].'">'.$proveedor['nombre_proveedor'].'</option>';
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <select class="form-control" id="id_especialidad">
+                    <option value="">Seleccione una especialidad</option>
+                    <?php 
+                        foreach($tipos_medicos as $tipo_medico){
+                            echo '<option value="'.$tipo_medico['id_tipo_medico'].'">'.$tipo_medico['nombre_tipo_medico'].'</option>';
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="nombre" placeholder="Nombre">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="apellido_paterno" placeholder="Apellido Paterno">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="apellido_materno" placeholder="Apellido Materno">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="telefono" placeholder="Telefono">
+            </div>
+            <div class="form-group">
+                <input type="email" class="form-control" id="correo" placeholder="correo">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="direccion" placeholder="Dirección">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="estado" placeholder="Estado">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="ciudad" placeholder="Ciudad">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="fax" placeholder="Fax">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="usuario" placeholder="Usuario">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="pass" placeholder="Contraseña">
+            </div>
+            <div class="form-group mb-0 mt-3 justify-content-end d-flex">
+                <div>
+                    <a class="btn btn-secondary ms-4" id="cancelar">Cancelar</a>
+                    <a class="btn btn-primary" id="submit_form" data-action="agregar_medico">Agregar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ php/v/0/tabla_medicos.php
@@ -0,0 +1,28 @@
+<?php
+if(session_status() == PHP_SESSION_NONE){
+    session_start();
+}
+include_once('../../m/SQLConexion.php');
+$sql = new SQLConexion();
+$resultado = $sql->obtenerResultado('CALL sp_select_medicos()');
+$total_resultado = count($resultado);
+
+if ($total_resultado > 0) {
+    for($i = 0; $i < $total_resultado; $i++){ ?>
+        <tr>
+            <th scope="row">#<?php echo $resultado[$i][9]; ?></th>
+            <td><?php echo $resultado[$i]['nombre']; ?></td>
+            <td style="display:flex;justify-content:space-around;" data-id="<?php echo $resultado[$i][9]; ?>">
+                <a href="javascript:void(0);" class="btn btn-success btn-sm" id="editar_proveedor">
+                    <i class="fa fa-edit"></i>
+                </a>
+                <a href="javascript:void(0);" class="btn btn-danger btn-sm" id="eliminar_registro">
+                    <i class="fa fa-trash"></i>
+                </a>
+            </td>
+        </tr>      
+    <?php }   
+}else{ ?>
+    <tr class="text-center" > <td colspan="4">No se encontraron registros</td> </tr>
+<?php }
+?>
