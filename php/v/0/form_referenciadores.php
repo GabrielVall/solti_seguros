2e10d6e5f27a025643de3a495b40d46f2eddd508
GabrielVall <gabrielvallejo2000@gmail.com>
2022-06-19 15:22:00 -0500

    Ajuste sesion

--- php/v/0/form_referenciadores.php
+++ php/v/0/form_referenciadores.php
@@ -4,7 +4,7 @@ if(session_status() == PHP_SESSION_NONE){
     // Si esta declarada la variable de sesion
 }
 // if string conatins 'clientes'
-if(!strpos($_SERVER['REQUEST_URI'], 'form_clientes') !== false){
+if(!strpos($_SERVER['REQUEST_URI'], 'form_referenciadores') !== false){
     $hash = $_SERVER['REQUEST_URI'];
     $hash = substr($hash, strrpos($hash, '/') + 1);
     // remove extension     
