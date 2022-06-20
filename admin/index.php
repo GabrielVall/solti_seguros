5571238c61c3d5d5983d9a76b44e21a861b111fd
GabrielVall <gabrielvallejo2000@gmail.com>
2022-06-19 19:33:01 -0500

    agregar template admin a repo

---
+++ admin/index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
+		<meta name="Author" content="Spruko Technologies Private Limited">
+		<meta name="Keywords" content="CodeIgniter Admin & Dashboard Template, Codeigniter Admin Template, Codeigniter admin panel, Codeigniter Bootstrap 5, Codeigniter admin template Bootstrap 5, Codeigniter Templates Using Bootstrap Admin, codeigniter admin, codeigniter dashboard, Admin & Dashboard Template, admin template, admin dashboard, admin panel, Bootstrap 5 admin template"/>
+		<title>Administrador</title>
+		<link rel="icon" href="nowa/nowa_demo/assets/img/brand/favicon.png" type="image/x-icon"/>
+		<link href="nowa/nowa_demo/assets/css/icons.css" rel="stylesheet">
+		<link id="style" href="nowa/nowa_demo/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="nowa/nowa_demo/assets/css/style.css" rel="stylesheet">
+		<link href="nowa/nowa_demo/assets/css/style-dark.css" rel="stylesheet">
+		<link href="nowa/nowa_demo/assets/css/style-transparent.css" rel="stylesheet">
+		<link href="nowa/nowa_demo/assets/css/skin-modes.css" rel="stylesheet" />
+		<link href="nowa/nowa_demo/assets/css/animate.css" rel="stylesheet">
+		<link href="nowa/nowa_demo/assets/switcher/css/switcher.css" rel="stylesheet"/>
+		<link href="nowa/nowa_demo/assets/switcher/demo.css" rel="stylesheet"/>
+		<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+		<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+		<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.js" integrity="sha512-bUg5gaqBVaXIJNuebamJ6uex//mjxPk8kljQTdM1SwkNrQD7pjS+PerntUSD+QRWPNJ0tq54/x4zRV8bLrLhZg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" integrity="sha512-DanfxWBasQtq+RtkNAEDTdX4Q6BPCJQ/kexi/RftcP0BcA4NIJPSi7i31Vl+Yl5OCfgZkdJmCqz+byTOIIRboQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	</head>
+	<body class="ltr main-body app sidebar-mini">
+		<div class="page">
+            <div>
+				<?php 
+				
+					include '../php/v/0/header.php';
+					include '../php/v/0/sidebar.php';
+				?>
+            </div>
+			<!-- main-content -->
+			<div class="main-content app-content">
+				<!-- container -->
+				<div class="main-container container-fluid">
+					<div class="breadcrumb-header justify-content-between">
+						<div class="left-content">
+						<span class="main-content-title mg-b-0 mg-b-lg-1">Vista inicio</span>
+						</div>
+					</div>
+					<div class="" id="contenido">
+					</div>
+                </div>
+				<!-- Container closed -->
+			</div>
+		</div>
+		<div class="modal fade" id="modaldemo3"  aria-modal="true" role="dialog">
+			<div class="modal-dialog modal-lg" role="document">
+				<div class="modal-content modal-content-demo">
+					
+				</div>
+			</div>
+		</div>
+		<!-- <div class="alert alert-solid-success alert-dismissible" role="alert" id="alerta">
+			<strong id="alert_top">Mensaje</strong> <span id="alert_text">Texto del mensaje</span>
+			<button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"><span aria-hidden="true">×</span></button>
+		</div> -->
+		<!-- End Page -->
+				<!-- Back-to-top -->
+		<a href="#top" id="back-to-top"><i class="las la-arrow-up"></i></a>
+		<!-- JQuery min js -->
+		<script src="nowa/nowa_demo/assets/plugins/jquery/jquery.min.js"></script>
+		<!-- Bootstrap js -->
+		<script src="nowa/nowa_demo/assets/plugins/bootstrap/js/popper.min.js"></script>
+		<script src="nowa/nowa_demo/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<!-- Moment js -->
+		<script src="nowa/nowa_demo/assets/plugins/moment/moment.js"></script>
+		<!-- P-scroll js -->
+		<!-- <script src="nowa/nowa_demo/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="nowa/nowa_demo/assets/plugins/perfect-scrollbar/p-scroll.js"></script> -->
+		<!-- eva-icons js -->
+		<script src="nowa/nowa_demo/assets/js/eva-icons.min.js"></script>
+		<!-- Sidebar js -->
+		<!-- <script src="nowa/nowa_demo/assets/plugins/side-menu/sidemenu.js"></script> -->
+		<!-- Internal Chart.Bundle js-->
+		<script src="nowa/nowa_demo/assets/plugins/chart_js/Chart.bundle.min.js"></script>
+		<!-- INTERNAL Apexchart js -->
+		<script src="nowa/nowa_demo/assets/js/apexcharts.js"></script>
+		<!--Internal Sparkline js -->
+		<script src="nowa/nowa_demo/assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+		<!--Internal  Perfect-scrollbar js -->
+		<!-- <script src="nowa/nowa_demo/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="nowa/nowa_demo/assets/plugins/perfect-scrollbar/p-scroll.js"></script> -->
+		<!--Internal  index js -->
+		<script src="nowa/nowa_demo/assets/js/index.js"></script>
+		<!-- Chart-circle js -->
+		<script src="nowa/nowa_demo/assets/js/circle-progress.min.js"></script>
+		<!-- Internal Data tables -->
+		<script src="nowa/nowa_demo/assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+		<script src="nowa/nowa_demo/assets/plugins/datatable/js/dataTables.bootstrap5.js"></script>
+		<script src="nowa/nowa_demo/assets/plugins/datatable/dataTables.responsive.min.js"></script>
+		<script src="nowa/nowa_demo/assets/plugins/datatable/responsive.bootstrap5.min.js"></script>
+		<!-- INTERNAL Select2 js -->
+		<script src="nowa/nowa_demo/assets/plugins/select2/js/select2.full.min.js"></script>
+		<script src="nowa/nowa_demo/assets/js/select2.js"></script>
+		<!-- Sticky js -->
+		<script src="nowa/nowa_demo/assets/js/sticky.js"></script>
+		<!-- Right-sidebar js -->
+		<!-- <script src="nowa/nowa_demo/assets/plugins/sidebar/sidebar.js"></script>
+		<script src="nowa/nowa_demo/assets/plugins/sidebar/sidebar-custom.js"></script> -->
+		<!-- Theme Color js -->
+		<script src="nowa/nowa_demo/assets/js/themecolor.js"></script>
+		<!-- custom js -->
+		<script src="nowa/nowa_demo/assets/js/custom.js"></script>
+		<!-- Switcher js -->
+		<!-- <script src="nowa/nowa_demo/assets/switcher/js/switcher.js"></script> -->
+		<script src="js/main.js?0.0"></script>
+</html>
---
+++ admin/js/main.js
@@ -0,0 +1,420 @@
+// Al cargar
+var hash = '';
+var estados = [];
+$(document).ready(function() {
+    cambiar_hash();
+    $(document).ajaxStart(function() {
+        NProgress.start();
+        NProgress.set(0.4);
+    });
+    $(document).ajaxComplete(function() {
+        NProgress.done();
+    });
+    // Al cambiar el hash
+    $(window).on('hashchange', function() {
+        cambiar_hash();
+    });
+    
+    function cambiar_hash(){
+        hash = window.location.hash;
+        hash = hash.substring(1);
+        if(hash == ''){
+            window.location.hash = '#inicio';
+        }else{
+            $('[href]').removeClass('active');
+            $('[href="#'+hash+'"]').addClass('active');
+            $.ajax({
+                url: '../php/v/0/'+hash+'.php',
+                success: function(data){
+                    $('#contenido').html(data);
+                    cargar_funciones_hash();
+                },error: function(data){
+                    window.location.hash = 'error';
+                    // Scroll bottom
+                    $('html, body').animate({
+                        scrollTop: $(document).height()
+                    }, 100);
+                }
+            });
+        }
+    }
+    function cargar_funciones_hash(){
+        cargar_estados_selects();
+        append_button_usuario();
+        append_button_pass();
+        cargar_tablas();
+        cargar_selects();
+    }
+    
+    function cargar_estados_selects(){
+        $.ajax({
+            url: 'js/estados-municipios.json',
+            success: function(data){
+                estados = data;
+                // Ceate options for selects
+                var options = '';
+                var n = 0;
+                for(var i in data){
+                    n++;
+                    options += '<option value="'+n+'">'+i+'</option>';
+                }
+                // Append options to selects
+                $('#estado').append(options);
+            }
+        });
+    }
+    $(document).on('change', '#estado', function(){
+        // Text for select
+        var text = $(this).find('option:selected').text();
+        var options = '';
+        var n = 0;
+        var length = estados[text].length;
+        for( var i = 0; i < length; i++){
+            n++;
+            console.log(estados[text][i]);
+            options += '<option value="'+n+'">'+estados[text][i]+'</option>';
+        }
+        $('#ciudad').html(options);
+    });
+    function cargar_tablas(){
+        $('table').DataTable({
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            }
+        });
+    }
+    function cargar_selects(){
+        $('select').select2({
+			placeholder: 'Escoger una opción',
+			searchInputPlaceholder: 'Buscar..',
+            "language": {
+                "noResults": function(){
+                    return "No se encontraron resultados";
+                }
+            },
+		});
+    }
+    function append_button_usuario(){
+        var btn = `
+        <button type="button" class="btn btn-light btn-sm mb-1 button_input" id="generar_usuario">
+            <i class="mdi mdi-account-convert" data-bs-toggle="tooltip" title="" data-bs-original-title="mdi-account-convert" aria-label="mdi-account-convert"></i>
+        </button>
+        `;
+        $('[for="usuario"]').append(btn);
+    }
+    function append_button_pass(){
+        var btn = `
+        <button type="button" class="btn btn-light btn-sm mb-1 button_input" id="generar_pass">
+            <i class="mdi mdi-key" data-bs-toggle="tooltip" title="" data-bs-original-title="mdi-key" aria-label="mdi-key"></i>
+        </button>
+        <button type="button" class="btn btn-primary btn-sm mb-1 button_input" id="copiar_pass"  style="padding-top:5px !important;" >
+            <i class="si si-docs" data-bs-toggle="tooltip" title="" data-bs-original-title="si-docs" aria-label="si-docs"></i>
+        </button>
+        `;
+        $('[for="pass"]').append(btn);
+    }
+    $(document).on('click', '#generar_usuario', function(e){
+        generar_usuario();
+    });
+    $(document).on('click', '#generar_pass', function(e){
+        $('#pass').val(generar_pass());
+    });
+    $(document).on('click', '#copiar_pass', function(e){
+          /* Get the text field */
+        var copyText = document.getElementById("pass");
+
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.value);
+
+        /* Alert the copied text */
+        alertify.success('Copiado al portapapeles: ' + copyText.value);
+    });
+    function generar_pass(){
+        var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+    }
+    // On focus
+    $(document).on('focus', '#usuario', function(e){
+        generar_usuario();
+    });
+
+    function generar_usuario(){
+        // Creamos un usuario aleatorio en base al nombre y apellido
+        var nombre = $('#nombre').val();
+        var apellido = $('#apellido_paterno').val();
+        if(nombre == '' || apellido == ''){
+            alertify.notify('Debe ingresar los datos del usuario');
+            return;
+        }
+        // if random 1,2
+        var random = Math.floor(Math.random() * 2) + 1;
+        if(random == 1){
+            var usuario = nombre.substring(0,1)+'_'+apellido;
+        }else{
+            var usuario = apellido.substring(0,2)+'_'+nombre;
+        }
+        var usuario = usuario.toLowerCase();
+        // Momento actual
+        var momento = new Date();
+        var momento = momento.getTime();
+        // Ultimos 2 caracteres
+        var ultimos_2 = momento.toString().substring(momento.toString().length-2);
+        // Primeros 2 caracteres
+        var primeros_2 = momento.toString().substring(0,2);
+        // Generamos el usuario
+        var usuario = usuario+ultimos_2+primeros_2;
+        $('#usuario').val(usuario);
+    }
+    $(document).on('click','#submit_form', function(e){
+        // get all inputs of the form
+        var inputs = $('.form-horizontal').find(':input');
+        // add in form data
+        var form_data = new FormData();
+        for(var i=0; i<inputs.length; i++){
+            form_data.append(inputs[i].id, inputs[i].value);
+        }
+        $.ajax({
+            url: '../php/c/0/'+$(e.target).data('action')+'.php',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            data: form_data,
+            success: function(data){
+                data = JSON.parse(data);
+                if(data.status == 'success'){
+                    vaciar_inputs();
+                    imprimir_vista('#tabla_consulta','tabla_'+hash+'.php',0);
+                    alertify.set('notifier','position', 'top-right');
+                    alertify.success('Registro agregado correctamente');
+                }
+            }
+        });
+    });
+
+    $(document).on('click','#cancelar', function(e){
+        vaciar_inputs();
+    });
+
+    function vaciar_inputs(){
+        $('body').find(':input').each(function(){
+            $(this).val('');
+        });
+    }
+
+    $(document).on('click','#cancelar_editar_cliente', function(e){
+        imprimir_vista('#form_cliente_content','form_clientes.php',0);
+    });
+
+    $(document).on('click','#cancelar_editar_especialidad', function(e){
+        imprimir_vista('#form_cliente_content','form_especialidades.php',0);
+    });
+
+    $(document).on('click', '#eliminar_registro', function(e){
+        alertify.confirm('Alerta', '¿Estas seguro de realizar esta acción?, ¡esto no podra deshacerse!.', function(){
+            var id_cliente = $(e.target).parent().data('id');
+            $.ajax({
+                url: '../php/c/0/eliminar_cliente.php',
+                type: 'POST',
+                data: {id_cliente: id_cliente},
+                success: function(data){
+                    data = JSON.parse(data);
+                    if(data.status == 'success'){
+                        $(e.target).parent().parent().remove();
+                    }
+                }
+            });
+        }, function(){
+            alertify.error('Acción cancelada');
+        });
+    });
+
+    $(document).on('click', '#editar_cliente', function(){
+        var id = $(this).parent().data('id');
+        imprimir_vista('#form_cliente_content','editar_cliente.php',id);
+    });
+
+    $(document).on('click', '#editar_especialidad', function(){
+        var id = $(this).parent().data('id');
+        imprimir_vista('#form_cliente_content','editar_especialidad.php',id);
+    });
+
+    $(document).on('click', '#editar_aseguradora', function(){
+        var id = $(this).parent().data('id');
+        imprimir_vista('#form_aseguradora_content','editar_aseguradora.php',id);
+    });
+
+    $(document).on('click', '#editar_proveedor', function(){
+        var id = $(this).parent().data('id');
+        imprimir_vista('#form_aseguradora_content','editar_proveedor.php',id);
+    });
+    $(document).on('click', '#editar_medico', function(){
+        var id = $(this).parent().data('id');
+        imprimir_vista('#form_medico_content','editar_medico.php',id);
+    });
+
+    $(document).on('click', '#editar_referenciador', function(){
+        var id = $(this).parent().data('id');
+        imprimir_vista('#form_cliente_content','editar_referenciador.php',id);
+    });
+
+    $(document).on('click', '#editar_cliente_form', function(){
+        var id = $(this).data('id');
+         // get all inputs of the form
+         var inputs = $('.form-horizontal').find(':input');
+         // add in form data
+         var form_data = new FormData();
+         for(var i=0; i<inputs.length; i++){
+             form_data.append(inputs[i].id, inputs[i].value);
+         }
+         form_data.append('id', id);
+         $.ajax({
+             url: '../php/c/0/editar_cliente.php',
+             type: 'POST',
+             processData: false,
+             contentType: false,
+             data: form_data,
+             success: function(data){
+                 data = JSON.parse(data);
+                 if(data.status == 'success'){
+                    if(data.tipo == 0){
+                        imprimir_vista('#form_cliente_content','form_clientes.php',0);
+                        imprimir_vista('#tabla_consulta','tabla_clientes.php',0);
+                    }else{
+                        imprimir_vista('#form_aseguradora_content','form_'+hash+'.php',0);
+                        imprimir_vista('#tabla_consulta','tabla_'+hash+'.php',0);
+                    }
+                     alertify.set('notifier','position', 'top-right');
+                     alertify.success('Registro modificado');
+                 }
+             }
+         });
+    });
+
+    $(document).on('click', '#editar_especialidad_form', function(){
+        var id = $(this).data('id');
+         // get all inputs of the form
+         var inputs = $('.form-horizontal').find(':input');
+         // add in form data
+         var form_data = new FormData();
+         for(var i=0; i<inputs.length; i++){
+             form_data.append(inputs[i].id, inputs[i].value);
+         }
+         form_data.append('id', id);
+         $.ajax({
+             url: '../php/c/0/editar_especialidad.php',
+             type: 'POST',
+             processData: false,
+             contentType: false,
+             data: form_data,
+             success: function(data){
+                 data = JSON.parse(data);
+                 if(data.status == 'success'){
+                    imprimir_vista('#form_cliente_content','form_especialidades.php',0);
+                    imprimir_vista('#tabla_consulta','tabla_especialidades.php',0);
+                    alertify.set('notifier','position', 'top-right');
+                    alertify.success('Registro modificado');
+                 }
+             }
+         });
+    });
+
+    $(document).on('click', '#editar_medico_form', function(){
+        var id = $(this).data('id');
+         // get all inputs of the form
+         var inputs = $('.form-horizontal').find(':input');
+        //  Agregar selects
+        var selects = $('.form-horizontal').find('select');
+         // add in form data
+         var form_data = new FormData();
+         for(var i=0; i<inputs.length; i++){
+             form_data.append(inputs[i].id, inputs[i].value);
+         }
+         for(var i=0; i<selects.length; i++){
+                form_data.append(selects[i].id, selects[i].value);
+         }
+         form_data.append('id', id);
+         $.ajax({
+             url: '../php/c/0/editar_medico.php',
+             type: 'POST',
+             processData: false,
+             contentType: false,
+             data: form_data,
+             success: function(data){
+                 data = JSON.parse(data);
+                 if(data.status == 'success'){
+                     imprimir_vista('#form_medico_content','form_medicos.php',0);
+                     imprimir_vista('#tabla_consulta','tabla_medicos.php',0);
+                     alertify.set('notifier','position', 'top-right');
+                     alertify.success('Registro modificado');
+                 }
+             }
+         });
+    });
+    
+
+    $(document).on('click', '#editar_referenciador_form', function(){
+        var id = $(this).data('id');
+         // get all inputs of the form
+         var inputs = $('.form-horizontal').find(':input');
+         // add in form data
+         var form_data = new FormData();
+         for(var i=0; i<inputs.length; i++){
+             form_data.append(inputs[i].id, inputs[i].value);
+         }
+         form_data.append('id', id);
+         $.ajax({
+             url: '../php/c/0/editar_referenciador.php',
+             type: 'POST',
+             processData: false,
+             contentType: false,
+             data: form_data,
+             success: function(data){
+                 data = JSON.parse(data);
+                 if(data.status == 'success'){
+                     imprimir_vista('#form_cliente_content','form_referenciadores.php',0);
+                     imprimir_vista('#tabla_consulta','tabla_referenciadores.php',0);
+                     alertify.set('notifier','position', 'top-right');
+                     alertify.success('Registro modificado');
+                 }
+             }
+         });
+    });
+
+    function imprimir_vista(id,file,datos){
+        $.ajax({
+            url: '../php/v/0/'+file,
+            type: 'POST',
+            data: {id: datos},
+            success: function(data){
+                $(id).html(data);
+            }
+        });
+    }
+
+});
