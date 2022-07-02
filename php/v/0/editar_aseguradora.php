1ef667b0772f509e38fe6e4ff771b0bc8012f3a9
GabrielVall <gabrielvallejo2000@gmail.com>
2022-07-01 23:41:06 -0500

    ajustes form

--- php/v/0/editar_aseguradora.php
+++ php/v/0/editar_aseguradora.php
@@ -4,6 +4,20 @@ include_once('../../m/SQLConexion.php');
 $sql = new SQLConexion();
 
 $cliente = $sql->obtenerResultado("CALL sp_select_aseguradora('".$_POST['id']."')");
+if($_SESSION['hash'] == 'aseguradoras'){
+    $hidden = array(
+        'nombre' => true,
+        'apellido_paterno' => true,
+        'apellido_materno' => true,
+        'correo' => true,
+        'usuario' => true,
+        'pass' => true,
+        'fax' => true,
+        'direccion' => true,
+        'ciudad' => true,
+        'fax' => true,
+    );
+}
 ?>
 <div class="card  box-shadow-0">
     <div class="card-header">
@@ -15,37 +29,40 @@ $cliente = $sql->obtenerResultado("CALL sp_select_aseguradora('".$_POST['id']."'
             <div class="form-group">
                 <input type="text" class="form-control" id="nombre_aseguradora" placeholder="Nombre aseguradora" value="<?php echo $cliente[0]['nombre_aseguradora']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['nombre'])){echo "display:none;";} ?>">
                 <input type="text" class="form-control" id="nombre" placeholder="Nombre" value="<?php echo $cliente[0]['nombre']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['apellido_paterno'])){echo "display:none;";} ?>">
                 <input type="text" class="form-control" id="apellido_paterno" placeholder="Apellido Paterno" value="<?php echo $cliente[0]['apellido_paterno']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['apellido_materno'])){echo "display:none;";} ?>">
                 <input type="text" class="form-control" id="apellido_materno" placeholder="Apellido Materno" value="<?php echo $cliente[0]['apellido_materno']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" >
                 <input type="text" class="form-control" id="telefono" placeholder="Telefono" value="<?php echo $cliente[0]['telefono']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['correo'])){echo "display:none;";} ?>">
                 <input type="email" class="form-control" id="correo" placeholder="correo" value="<?php echo $cliente[0]['mail']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['direccion'])){echo "display:none;";} ?>">
                 <input type="text" class="form-control" id="direccion" placeholder="Dirección" value="<?php echo $cliente[0]['direccion']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['ciudad'])){echo "display:none;";} ?>">
                 <label for="ciudad">Ciudad</label>
+                    <?php if(isset($hidden['ciudad'])){ ?>
+                        <option value="0" selected>Sin valor</option>
+                    <?php } ?>
                 <select class="form-control" id="ciudad" data-val="<?php echo $cliente[0]['id_ciudad']; ?>">
                     <option value="">Seleccione una ciudad</option>
                 </select>
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['fax'])){echo "display:none;";} ?>">
                 <input type="text" class="form-control" id="fax" placeholder="Fax" value="<?php echo $cliente[0]['fax']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['usuario'])){echo "display:none;";} ?>">
                 <input type="text" class="form-control" id="usuario" placeholder="Usuario" value="<?php echo $cliente[0]['usuario']; ?>">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['pass'])){echo "display:none;";} ?>">
                 <input type="password" class="form-control" id="pass" placeholder="Contraseña" >
             </div>
             <div class="form-group mb-0 mt-3 justify-content-end d-flex">
--- php/v/0/form_aseguradoras.php
+++ php/v/0/form_aseguradoras.php
@@ -13,7 +13,20 @@ if(!strpos($_SERVER['REQUEST_URI'], 'form_aseguradoras') !== false){
 }else{
     $hash = $_SESSION['hash'];
 }
-
+if($_SESSION['hash'] == 'aseguradoras'){
+    $hidden = array(
+        'nombre' => true,
+        'apellido_paterno' => true,
+        'apellido_materno' => true,
+        'correo' => true,
+        'usuario' => true,
+        'pass' => true,
+        'fax' => true,
+        'direccion' => true,
+        'ciudad' => true,
+        'fax' => true,
+    );
+}
 // Get url
 // get text after last slash
 ?>
@@ -25,50 +38,53 @@ if(!strpos($_SERVER['REQUEST_URI'], 'form_aseguradoras') !== false){
     <div class="card-body pt-0">
         <div class="form-horizontal" >
             <div class="form-group">
-                <label for="nombre">Nombre de la aseguradora</label>
+                <label for="nombre" >Nombre de la aseguradora</label>
                 <input type="text" class="form-control" id="nombre_aseguradora" placeholder="Nombre de la aseguradora">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['nombre'])){echo "display:none;";} ?>">
                 <label for="nombre">Nombre</label>
-                <input type="text" class="form-control" id="nombre" placeholder="Nombre">
+                <input type="text" class="form-control" <?php if(isset($hidden['nombre'])){echo 'value="Sin Valor"';} ?> id="nombre" placeholder="Nombre">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['apellido_paterno'])){echo "display:none;";} ?>">
                 <label for="apellido_paterno">Apellido paterno</label>
-                <input type="text" class="form-control" id="apellido_paterno" placeholder="Apellido Paterno">
+                <input type="text" class="form-control" id="apellido_paterno" <?php if(isset($hidden['apellido_paterno'])){echo 'value="Sin Valor"';} ?> placeholder="Apellido Paterno">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['apellido_materno'])){echo "display:none;";} ?>">
                 <label for="apellido_materno">Apellido materno</label>
-                <input type="text" class="form-control" id="apellido_materno" placeholder="Apellido Materno">
+                <input type="text" class="form-control" id="apellido_materno" <?php if(isset($hidden['apellido_materno'])){echo 'value="Sin Valor"';} ?> placeholder="Apellido Materno">
             </div>
             <div class="form-group">
                 <label for="telefono">Telefono</label>
                 <input type="text" class="form-control" id="telefono" placeholder="Telefono">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['correo'])){echo "display:none;";} ?>">
                 <label for="correo">Correo electronico</label>
-                <input type="email" class="form-control" id="correo" placeholder="correo">
+                <input type="email" class="form-control" id="correo"  <?php if(isset($hidden['correo'])){echo 'value="Sin Valor"';} ?> placeholder="correo">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['direccion'])){echo "display:none;";} ?>">
                 <label for="direccion">Dirección</label>
-                <input type="text" class="form-control" id="direccion" placeholder="Dirección">
+                <input type="text" class="form-control" <?php if(isset($hidden['direccion'])){echo 'value="Sin Valor"';} ?> id="direccion" placeholder="Dirección">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['ciudad'])){echo "display:none;";} ?>">
                 <label for="ciudad">Ciudad</label>
                 <select class="form-control" id="ciudad">
+                    <?php if(isset($hidden['ciudad'])){ ?>
+                        <option value="0" selected>Sin valor</option>
+                    <?php } ?>
                     <option value="">Seleccione una ciudad</option>
                 </select>
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['fax'])){echo "display:none;";} ?>">
                 <label for="fax">Fax</label>
-                <input type="text" class="form-control" id="fax" placeholder="Fax">
+                <input type="text" class="form-control" id="fax" <?php if(isset($hidden['fax'])){echo 'value="Sin Valor"';} ?> placeholder="Fax">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['usuario'])){echo "display:none;";} ?>">
                 <label for="usuario">Nombre de usuario</label>
-                <input type="text" class="form-control" id="usuario" placeholder="Usuario">
+                <input type="text" class="form-control" id="usuario" <?php if(isset($hidden['usuario'])){echo 'value="Sin Valor"';} ?> placeholder="Usuario">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="<?php if(isset($hidden['pass'])){echo "display:none;";} ?>">
                 <label for="pass">Contraseña</label>
-                <input type="password" class="form-control" id="pass" placeholder="Contraseña">
+                <input type="password" class="form-control" id="pass" <?php if(isset($hidden['pass'])){echo 'value="Sin Valor"';} ?> placeholder="Contraseña">
             </div>
             <div class="form-group mb-0 mt-3 justify-content-end d-flex">
                 <div>
